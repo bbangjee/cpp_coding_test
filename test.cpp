f95192ca585cc0d599f38a0fed407e12554b21f5
bbangjee <qkdwl9584@naver.com>
2026-01-21 11:53:04 +0900

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "a";
+
+    for (int i = 0; i < s.size() - 2; i++) {
+        cout << "LOOP" << i << endl;
+    }
+
+    return 0;
+}
+
