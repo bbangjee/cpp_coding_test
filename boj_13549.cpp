f155bafe61f08794fa4d24e202a675b668fc71ec
bbangjee <qkdwl9584@naver.com>
2026-01-27 17:32:16 +0900

    boj_13549.cpp 파일 추가: BFS를 사용하여 최단 경로 계산 구현

---
+++ boj_13549.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int visited[100002];
+int dir[2] = { -1, 1};
+// 1초 후에 x - 1 또는 x + 1
+// 0초 후에 x * 2
+int n, k;
+queue<int> q;
+int main() {
+    cin >> n >> k;
+    q.push(n);
+    visited[n] = 1;
+    while (!q.empty()) {
+        int curr = q.front();
+        if (curr == k) {
+            cout << visited[curr] - 1 << '\n';
+            return 0;
+        }
+        q.pop();
+        int new_pos = curr * 2;
+        if (new_pos <= 100000 && visited[new_pos] == 0) {
+            q.push(new_pos);
+            visited[new_pos] = visited[curr];
+        }
+        for (int i = 0; i < 2; i++) {
+            new_pos = curr + dir[i];
+            if (new_pos <= 100000 && new_pos >= 0 && visited[new_pos] == 0) {
+            q.push(new_pos);
+            visited[new_pos] = visited[curr] + 1;
+            }
+        }
+
+    } 
+
+    return 0;
+}
